277b091f6ef29d353b37c738bccfac62fe0aa4b5
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-12-19 09:43:18 -0500

    test: add code coverage

---
+++ nyc.config.cjs
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  all: true,
+  'check-coverage': true,
+  exclude: ['src/__tests__/**/*.*'],
+  extends: '@istanbuljs/nyc-config-typescript',
+  include: ['src/**/*'],
+  reporter: ['html', 'lcov', 'text', 'text-summary'],
+}
